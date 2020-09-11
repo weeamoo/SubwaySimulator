7e4ad13cdd9af339939bae3723edf384a9891f34
weeamoo <github@surfinontheinter.net>
2020-09-11 18:13:30 -0500

    level load
    foundation for level loader

--- js/util.js
+++ js/util.js
@@ -1,3 +1,6 @@
-function loadLevel ( level, physics, css, script ) {
-	document.getElementById('world').innerHTML = level;
+function loadLevel (level) {
+
+	//level is path to level ex. /js/levels/test.test1.js
+	
+	document.getElementById('lvlLoader').src = level;
 }

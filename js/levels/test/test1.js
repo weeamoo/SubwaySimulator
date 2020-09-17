6d853326405866c40fef477629b3242fa900f0b3
Weeamoo <github@surfinontheinter.net>
2020-09-17 10:52:00 -0500

    Update test1.js

--- js/levels/test/test1.js
+++ js/levels/test/test1.js
@@ -1,6 +1,21 @@
 /* this function is essentially the geometry sliced by the if or switch statement */
 
 function getNearestRail (x, y) {
+	
+	switch (x) {
+    		case (x < 5):
+        		alert("less than five");
+        		break;
+    		case (x < 9):
+        		alert("between 5 and 8");
+        		break;
+    		case (x < 12):
+        		alert("between 9 and 11");
+        		break;
+    		default:
+        		alert("none");
+        		break;
+}
 
 
 	/* outputs y coord of nearest rail */

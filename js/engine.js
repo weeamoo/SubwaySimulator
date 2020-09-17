850aa905df8c84c3af54b544271fe7c456ee0440
weeamoo <github@surfinontheinter.net>
2020-09-17 02:43:30 -0500

    skeleton for test level and engine

---
+++ js/engine.js
@@ -0,0 +1,6 @@
+/* make function here to call get nearest rail and  then have it decide weather to warp the subway up a bit to be on top of the rail or let it fall
+
+	Condition	Return
+	above rail	0
+	on rail		1
+	in rail		1
---
+++ js/levels/test/test1.js
@@ -0,0 +1,9 @@
+/* this function is essentially the geometry sliced by the if or switch statement */
+
+function getNearestRail (x, y) {
+
+
+	/* outputs y coord of nearest rail */
+	return outY
+}
+

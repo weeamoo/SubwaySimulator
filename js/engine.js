faae817e9bd0274de1b771a1014c059125a3912c
weeamoo <github@surfinontheinter.net>
2020-09-26 21:26:16 -0500

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -1,8 +1,14 @@
+/* write code here to find screen size and save it to vars */
+
 /* make function here to call get nearest rail and  then have it decide weather to warp the subway up a bit to be on top of the rail or let it fall
 
 	Condition	Return
 	above rail	0
 	on rail		1
 	in rail		1
+
+*/
+
+/* make function here to move car to specific coords on map in game units and translate them to whatever is needed for tha browser
 
 */

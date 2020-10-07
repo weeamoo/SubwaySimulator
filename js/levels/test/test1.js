05f9c5e00cefa6f48941ff82fa76405122a7f155
weeamoo <github@surfinontheinter.net>
2020-10-07 05:21:03 -0500

    Update test1.js
    some values for this

--- js/levels/test/test1.js
+++ js/levels/test/test1.js
@@ -1,16 +1,27 @@
 /* this function is essentially the geometry sliced by the if or switch statement */
 
 function getNearestRail (x, y) {
+
+	var outY;
 	
 	switch (x) {
-    		case (x < 5):
-        		alert("less than five");
+
+		/*x coord starts at zero */
+
+    		case (x < 900):
+        		
+    			outY = 500;
+
         		break;
-    		case (x < 9):
-        		alert("between 5 and 8");
+    		case (x < 1100):
+        		
+    			outY = x + 500;
+
         		break;
-    		case (x < 12):
-        		alert("between 9 and 11");
+    		case (x < 1300):
+        		
+    			outY = 700
+
         		break;
     		default:
         		alert("none");

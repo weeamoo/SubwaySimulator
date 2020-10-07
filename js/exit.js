f3e431d688844654ba5b16c0ca6be5ab87898074
weeamoo <github@surfinontheinter.net>
2020-10-07 16:51:53 -0500

    exit button

---
+++ js/exit.js
@@ -0,0 +1,30 @@
+function exit () {
+	document.getElementById("pauseMenuH1").innerHTML = "c";
+	document.getElementById("title").innerHTML = "c";
+	sleep(200);
+	document.getElementById("pauseMenuH1").innerHTML = "cy";
+	document.getElementById("title").innerHTML = "cy";
+	sleep(200);
+	document.getElementById("pauseMenuH1").innerHTML = "cya";
+	document.getElementById("title").innerHTML = "cya";
+	sleep(200);
+	document.getElementById("pauseMenuH1").innerHTML = "cya ";
+	document.getElementById("title").innerHTML = "cya ";
+	sleep(200);
+	document.getElementById("pauseMenuH1").innerHTML = "cya l";
+	document.getElementById("title").innerHTML = "cya l";
+	sleep(200);
+	document.getElementById("pauseMenuH1").innerHTML = "cya l8";
+	document.getElementById("title").innerHTML = "cya l8";
+	sleep(200);
+	document.getElementById("pauseMenuH1").innerHTML = "cya l8r";
+	document.getElementById("title").innerHTML = "cya l8r";
+}
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}

ad192b9e915502239ea739f0f0d670edbae4f2eb
weeamoo <github@surfinontheinter.net>
2020-10-07 19:08:29 -0500

    Update everyFrame.js

--- js/everyFrame.js
+++ js/everyFrame.js
@@ -1,8 +1,26 @@
 //runs every frame
 function everyFrame () {
 
+	keyActions();
+
+	frameMovement();
+
+	frameSpeedEnv();
+
 	//calls function after one frame has passed
 	requestAnimationFrame(everyFrame);
+}
+
+function keyActions () {
+
+}
+
+function frameMovement () {
+
+}
+
+function frameSpeedEnv () {
+	
 }
 
 //inital call to function to start eveything up

8585f93c670ce1964f6b5ab0406fd000d283ec55
weeamoo <github@surfinontheinter.net>
2020-10-08 23:59:48 -0500

    Update render.js

--- js/render.js
+++ js/render.js
@@ -1,6 +1,11 @@
+
+//places subway car on screen using game coords
 function UpdateSubPos (xPass, yPass) {
 
 // translates game units to percent of screen
 	xPass = xPass / 16;
 	yPass = yPass / 9;
+
+	document.getElementById("playerCarDiv").style.left = xPass + "%";
+	document.getElementById("playerCarDiv").style.bottom = yPass + "%"
 }
